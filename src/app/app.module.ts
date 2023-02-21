05747f7ca1f1abfb165d52e94f100ec14f42e7a3
laska-adief <laskaadief.la@gmail.com>
2023-02-21 18:41:36 +0700

    [Employee Management] Create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+import { EmployeeAddEditComponent } from './employee-add-edit/employee-add-edit.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeDashboardComponent,
+    EmployeeListComponent,
+    EmployeeDetailComponent,
+    EmployeeAddEditComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
