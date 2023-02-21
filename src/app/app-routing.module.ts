a73177b729f04835a237a33f05a1cb7a319e75f6
laska-adief <laskaadief.la@gmail.com>
2023-02-21 18:42:20 +0700

    [Employee Management] Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeAddEditComponent } from './employee-add-edit/employee-add-edit.component';
+import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'dashboard', pathMatch:'full'},
+  {path:'dashboard', component:EmployeeDashboardComponent},
+  {path:'list', component:EmployeeListComponent},
+  {path:'add', component:EmployeeAddEditComponent},
+  {path:'edit/:id', component:EmployeeAddEditComponent},
+  {path:'detail/:id', component:EmployeeDetailComponent},
+  {path:'**', redirectTo:'dashboard'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
