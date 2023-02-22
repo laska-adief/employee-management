f67b366602d5f9c210acf9b5a3feb7d63eff32a5
laska-adief <laskaadief.la@gmail.com>
2023-02-22 09:47:33 +0700

    [Employee Management] Query get data employee

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Employee } from '../employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  readonly URL:string = 'https://63f48a7a55677ef68bbe3578.mockapi.io/api/employee';
+
+  constructor(private http:HttpClient) { }
+
+  getAllEmployee() {
+    return this.http.get<Employee[]>(this.URL);  
+  }
 }
