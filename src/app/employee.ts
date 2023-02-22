fa27839317200d6a1477743198e32f245cbe76e2
laska-adief <laskaadief.la@gmail.com>
2023-02-22 09:49:00 +0700

    [Employee Management] Interface employee

---
+++ src/app/employee.ts
@@ -0,0 +1,16 @@
+export interface Employee {
+  "prefix": string,
+  "firstName": string,
+  "lastName": string,
+  "fullName": string,
+  "email": string,
+  "gender": string,
+  "birthDate": string,
+  "phone": string,
+  "avatar": string,
+  "street": string,
+  "city": string,
+  "country": string,
+  "jobTitle": string,
+  "id": string
+  }
